d2e7631a75eab268820499662afd0aa7d2328aae
lopes <lopes@arquia.com>
2020-05-06 18:11:55 -0300

    creating project, finalized api repository search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(()=>{
+    api.get('repositories').then((response)=>{
+      console.log('response =>',response);
+      setRepositories(response.data);
+    })
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+    setRepositories([...repositories, `Novo repositorio ${Date.now()}`]);
   }
 
   async function handleRemoveRepository(id) {
@@ -14,13 +24,15 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repositorio => 
+        <li key={repositorio.id}>
+         {repositorio.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repositorio.id)}>
             Remover
           </button>
-        </li>
+        </li>)}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
